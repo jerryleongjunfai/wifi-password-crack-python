c828e8275e5f7da958916da21f7f970597e0f6ec
Jerry Leong <80385434+jerryleongjunfai@users.noreply.github.com>
2021-09-30 16:20:23 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import subprocess
+
+# Get metadata of your wifi
+data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split(
+    '\n')
+
+# Extract available Wi-Fi profiles names
+profile = []
+for i in data:
+    if "All User Profile" in i:
+        profile.append(i.split(":")[1][1:-1])
+
+for i in profile:
+    try:  # Extract Metadata of each profile
+        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles', i, "key=clear"]).decode('utf-8',
+                                                                                                        errors="backslashreplace").split(
+            '\n')
+        result = []
+
+        # Extract passwords from that metadata
+        for b in results:
+            if "Key Content" in b:
+                result.append(b.split(":")[1][1:-1])
+        # Print The Passwords in beautify way
+        try:
+            print("{:<30}| {:<}".format(i, result[0]))
+        except Exception as e:
+            print("{:<30}| {:<}".format(i, ""))
+    except Exception as e:
+        print("{:<30}| {:<}".format(i, "ERROR OCCURED"))
